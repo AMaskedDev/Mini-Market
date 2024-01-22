4ac4e8c1b43fe9970bf7a9904f603cd11fb2b792
masked <amaskeddev@gmail.com>
2024-01-22 21:50:56 +0200

    Finished removing products, took less time than expected :>

--- main.py
+++ main.py
@@ -66,14 +66,14 @@ class Main:
 
         # Storage Management Buttons
         self.add_product = customtkinter.CTkButton(self.header_frame, text="Add Product", corner_radius=0,
-                                                   fg_color="#47A641", hover_color="#3E9338")
+                                                   fg_color="#47A641", hover_color="#3E9338", command=lambda: Add_Product())
         self.add_product.pack(side="left", padx=(10, 0))
 
         self.remove_product = customtkinter.CTkButton(self.header_frame, text="Remove Product", corner_radius=2,
-                                                      fg_color="#D94B4B", hover_color="#B23D3D")
+                                                      fg_color="#D94B4B", hover_color="#B23D3D", command=lambda: Remove_Product())
         self.remove_product.pack(side="left", padx=(10, 0))
 
-        self.view_product = customtkinter.CTkButton(self.header_frame, text="View Products", corner_radius=2, )
+        self.view_product = customtkinter.CTkButton(self.header_frame, text="View Products", corner_radius=2)
         self.view_product.pack(side="left", padx=(10, 0))
 
         # Starting other methods
@@ -87,6 +87,96 @@ class Main:
 
         self.header_time.configure(text=current_time)
         self.header_time.after(1000, self.clock)
+
+
+class Remove_Product:
+    def __init__(self):
+        # Variables
+        self.application = None
+        self.barcode = None
+        self.product_exists = False
+
+        # Functions
+        self.BarcodeGUI()
+
+    def BarcodeGUI(self):
+        self.root = customtkinter.CTk()
+        self.root.geometry("500x250")
+        self.root.title("Product Scanning")
+        self.root.resizable(False, False)
+
+        # Header Frame
+        self.header_frame = customtkinter.CTkFrame(self.root, height=100, corner_radius=0)
+        self.header_frame.pack(fill="x", pady=(0, 30))
+        #
+        # Barcode input
+        self.product_id = customtkinter.CTkEntry(self.root, placeholder_text="Product ID", font=("calibri", 20),
+                                                 width=250, height=35)
+        self.product_id.pack(pady=(10, 5))
+
+        # Continue button
+        self.continue_button = customtkinter.CTkButton(
+            self.root, text="Continue", font=("calibri", 18), command=self.Checkpoint_To_Info,
+            width=250, height=35, fg_color="#47A641", hover_color="#3E9338", corner_radius=2
+        )
+        self.continue_button.pack(pady=(0, 4))
+
+        self.product_id.bind("<Return>", self.Checkpoint_To_Info)
+
+        # Time
+        self.header_clock = customtkinter.CTkLabel(self.header_frame, text="0:00", font=("calibri", 28))
+        self.header_clock.pack(side="right", pady=(12, 12), padx=(0, 15))
+
+        self.Clock()
+
+        self.root.mainloop()
+
+    def Checkpoint_To_Info(self, event=None):
+        self.barcode = self.product_id.get()
+
+        # Checking if the barcode is valid
+        if self.barcode == "":
+            messagebox.showerror("Checkpoint", "Please provide a valid product ID.")
+        else:
+            # Checking if it exists in the storage
+            data = None
+            with open("products.json", "r") as temp:
+                data = json.load(temp)
+
+            if self.barcode in data:
+                self.product_exists = True
+            else:
+                self.product_exists = False
+
+            # Displaying the info gui
+            self.RemoveProduct()
+            self.root.destroy()
+
+    def RemoveProduct(self, event=None):
+        # Temporary variables
+        file_data = None
+
+        # Loading the json file to a temporary data variable
+        with open("products.json", "r") as file:
+            file_data = json.load(file)
+
+        # Checking if the product exists
+        if self.barcode in file_data:
+            # Removing the whole `barcode` product
+            del file_data[self.barcode]
+
+            with open("products.json", "w") as file:
+                json.dump(file_data, file)
+
+            messagebox.showinfo("Product", "Product was removed from storage.")
+
+        else:
+            messagebox.showinfo("Product", "Product was not found in storage to remove.")
+    def Clock(self):
+        current_time = time.strftime("%I:%M %p")
+
+        self.header_clock.configure(text=current_time)
+        self.header_clock.after(1000, self.Clock)
 
 
 class Add_Product:
@@ -256,4 +346,4 @@ class Add_Product:
         self.header_clock.after(1000, self.Clock)
 
 
-Add_Product()
+Main()
