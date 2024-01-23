1b8004865f42201b401e3a4700cffdeefd513da5
AMaskedDev <153927002+AMaskedDev@users.noreply.github.com>
2024-01-23 17:24:56 +0200

    Finished the project

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import tkinter
 import json
 import time
 from CTkTable import *
+import atexit
 
 customtkinter.set_appearance_mode("light")
 
@@ -27,7 +28,6 @@ class Main:
         self.application.title("Mini Market")
 
         self.application.geometry("1050x600")
-        self.application.resizable(False, False)
 
         # Header Frame
         self.header_frame = customtkinter.CTkFrame(self.application, height=100,
@@ -38,7 +38,7 @@ class Main:
         self.product_list_frame.pack(pady=(25, 15), padx=(15, 15), fill=tkinter.BOTH, expand=True)
 
         # Buying treeview
-        self.products_list = CTkTable(self.product_list_frame, row=0, column=0, values=[["Amount", "Product", "Cost"],],
+        self.products_list = CTkTable(self.product_list_frame, row=0, column=0, values=[["Amount", "Product", "Cost"], ],
                                       font=("calibri", 20))
         self.products_list.pack(fill=tkinter.X)
 
@@ -50,16 +50,22 @@ class Main:
 
         # Buttons
         self.checkin_button = customtkinter.CTkButton(
-            self.application, text="Check in", font=("calibri", 18),
-            width=250, height=35, fg_color="#47A641", hover_color="#3E9338", corner_radius=2
+            self.application, text="Add Product", font=("calibri", 18),
+            width=250, height=35, corner_radius=2, command=self.AddProduct
         )
         self.checkin_button.pack(pady=(0, 4))
 
-        self.clear_button = customtkinter.CTkButton(
-            self.application, text="Clear order", font=("calibri", 18),
-            fg_color="#D94B4B", hover_color="#B23D3D", width=250, height=35, corner_radius=2
+        self.checkin_button = customtkinter.CTkButton(
+            self.application, text="Check in", font=("calibri", 18),
+            width=250, height=35, fg_color="#47A641", hover_color="#3E9338", corner_radius=2, command=self.Checkin
         )
-        self.clear_button.pack(pady=(0, 5))
+        self.checkin_button.pack(pady=(0, 4))
+
+        self.checkin_button = customtkinter.CTkButton(
+            self.application, text="Clear", font=("calibri", 18),
+            width=250, height=35, fg_color="#AB2525", hover_color="#942020", corner_radius=2, command=self.Checkin
+        )
+        self.checkin_button.pack(pady=(0, 15))
 
         # Time
         self.header_time = customtkinter.CTkLabel(self.header_frame, text="0:00",
@@ -82,10 +88,37 @@ class Main:
 
         # Starting other methods
         self.clock()
+
         self.barcode_input.bind("<Return>", self.AddProduct)
+        self.application.protocol("WM_DELETE_WINDOW", self.ClearList)
+        atexit.register(self.ClearList)
 
         # Application rendering loop
         self.application.mainloop()
+
+    def Checkin(self):
+        if self.total_cost > 0:
+            messagebox.showinfo("Check in", f"The total cost for every product is €{self.total_cost} \nThank you!")
+
+            # Reset the runtime back to available when closing the window
+            data = None
+            with open("products.json", "r") as file:
+                data = json.load(file)
+                for product_data in data.values():
+                    product_data["runtime"] = product_data["available"]
+
+            with open("products.json", "w") as file:
+                json.dump(data, file)
+
+            self.products_list.destroy()
+            self.products_list = CTkTable(self.product_list_frame, row=0, column=0, values=[["Amount", "Product", "Cost"], ],
+                                          font=("calibri", 20))
+            self.products_list.pack(fill=tkinter.X)
+
+            self.barcode_input.delete(0, tkinter.END)
+            self.barcode_input.focus_set()
+        else:
+            messagebox.showinfo("Error", "Couldn't checkout. \nNo products to buy!")
 
     def AddProduct(self, event=None):
         # Temporary variables
@@ -97,7 +130,6 @@ class Main:
         self.barcode_input.focus_set()
 
         # Adding to treeview
-
         if len(barcode) > 0:
             if os.path.isfile("products.json"):
                 with open("products.json", "r") as file:
@@ -108,19 +140,44 @@ class Main:
                     amount = simpledialog.askinteger("Amount", "How many products?")
 
                     if amount > 0:
-                        product = data[barcode]["name"]
-                        cost = data[barcode]["cost"]
+                        runtime = data[barcode].get("runtime", 0)  # Get the current runtime or default to 0
+                        if runtime >= amount:
+                            data[barcode]["runtime"] -= amount
 
-                        self.total_cost = data[barcode]["cost"] * amount
-                        self.products_list.add_row(values=[f"{amount}", f"{product}", f"{cost}"])
+                            product = data[barcode]["name"]
+                            cost = data[barcode]["cost"]
 
+                            self.total_cost += data[barcode]["cost"] * amount
+                            self.products_list.add_row(values=[f"{amount}", f"{product}", f"{cost * amount}"])
+
+                            # Update runtime in the file
+                            with open("products.json", "w") as file:
+                                json.dump(data, file)
+
+                        else:
+                            messagebox.showerror("Error", "Insufficient products in the storage.")
                     else:
                         messagebox.showerror("Error", "Invalid product amount")
-
-
-
+                else:
+                    messagebox.showerror("Error", f"Product with barcode {barcode} not found in storage.")
+            else:
+                messagebox.showerror("Error", "No products.json file found.")
         else:
             messagebox.showerror("Error", "Please use a valid product ID.")
+
+    def ClearList(self):
+        # Reset the runtime back to available when closing the window
+        data = None
+        with open("products.json", "r") as file:
+            data = json.load(file)
+            for product_data in data.values():
+                product_data["runtime"] = product_data["available"]
+
+        with open("products.json", "w") as file:
+            json.dump(data, file)
+
+        # Close the window
+        self.application.destroy()
 
     def clock(self):
         current_time = time.strftime("%I:%M %p")
@@ -131,7 +188,56 @@ class Main:
 
 class View_Products():
     def __init__(self):
-        pass
+        # Initializing the base window
+        self.application = customtkinter.CTk()
+        self.application.title("Mini Market")
+
+        self.application.geometry("1050x600")
+
+        # Header Frame
+        self.header_frame = customtkinter.CTkFrame(self.application, height=100,
+                                                   corner_radius=0)
+        self.header_frame.pack(fill="x")
+
+        self.product_list_frame = customtkinter.CTkScrollableFrame(self.application)
+        self.product_list_frame.pack(pady=(25, 15), padx=(15, 15), fill=tkinter.BOTH, expand=True)
+
+        # Buying treeview
+        self.products_list = CTkTable(self.product_list_frame, row=0, column=0, values=[["Product", "Cost", "Available"], ],
+                                      font=("calibri", 20))
+        self.products_list.pack(fill=tkinter.X)
+
+        # Time
+        self.header_time = customtkinter.CTkLabel(self.header_frame, text="0:00",
+                                                  font=("calibri", 28))
+        self.header_time.pack(side="right", pady=(12, 12), padx=(0, 15))
+
+        # Starting other methods
+        self.clock()
+        self.AddProducts()
+
+        # Application rendering loop
+        self.application.mainloop()
+
+    def AddProducts(self):
+        data = None
+
+        if os.path.isfile("products.json"):
+            with open("products.json", "r") as file:
+                data = json.load(file)
+
+            for barcode, product_data in data.items():
+                product_name = product_data.get("name", "")
+                product_cost = product_data.get("cost", "")
+                product_available = product_data.get("available", "")
+
+                self.products_list.add_row(values=[f"{product_name}", f"{product_cost}", f"{product_available}"])
+
+    def clock(self):
+        current_time = time.strftime("%I:%M %p")
+
+        self.header_time.configure(text=current_time)
+        self.header_time.after(1000, self.clock)
 
 
 class Remove_Product:
@@ -344,47 +450,52 @@ class Add_Product:
 
         self.application.mainloop()
 
-    def AddProduct(self, event=None):
-        # Getting entries input
+    def AddProduct(self):
         product_name = self.product_name.get()
         product_cost = self.product_cost.get()
         product_available = self.product_available.get()
 
-        # Temporary variables
-        file_data = None
+        data = None
+
+        try:
+            int(product_cost)
+            int(product_available)
+        except ValueError:
+            messagebox.showerror("Error", "Use only numbers in cost and availability.")
+            return  # Exit the function if there's an error
+
         product_data = {
             self.barcode: {
                 "name": product_name,
-                "cost": product_cost,
-                "available": product_available,
-                "runtime": product_available
+                "cost": int(product_cost),
+                "available": int(product_available),
+                "runtime": int(product_available)
             }
         }
 
-        # Loading the json file to a temporary data variable
-        with open("products.json", "r") as file:
-            file_data = json.load(file)
+        try:
+            with open("products.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            data = {}
+            messagebox.showerror("Error", "An error occurred while opening file products.json")
 
-        # Checking if the product exists
-        if self.barcode in file_data:
-            # Updating the `old` with the `new` info
-            file_data[self.barcode].update(product_data)
-
-            with open("products.json", "w") as file:
-                json.dump(product_data, file)
-                file.close()
-
-            messagebox.showinfo("Product", "Product was edited to storage.")
-
+        # Check if the product_id already exists in the data
+        if self.barcode in data:
+            # Product exists, update cost and availability
+            data[self.barcode]["cost"] = int(product_cost)
+            data[self.barcode]["available"] = int(product_available)
         else:
-            # Creating a new product
-            with open("products.json", "w") as file:
-                json.dump(product_data, file)
-                file.close()
+            # Product doesn't exist, add new product information
+            data.update(product_data)
 
-                messagebox.showinfo("Product", "New product was added to storage.")
+        with open("products.json", "w") as file:
+            json.dump(data, file)
 
-        self.application.destroy()
+        if product_cost is not None and product_available is not None and product_available is not None:
+            messagebox.showinfo("Valid Product Info", f"Product {product_name} was added to storage successfully.")
+        else:
+            messagebox.showerror("Invalid Product Info", f"Product was not added, invalid product information.")
 
     def Clock(self):
         current_time = time.strftime("%I:%M %p")
